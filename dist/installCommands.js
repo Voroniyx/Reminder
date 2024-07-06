13c4a2ec5b847acb788759ef5a0e3755bdb83a23
Voroniyx <julian.ruge@icloud.com>
2024-07-07 00:35:04 +0200

    dist hinzugefügt

--- dist/installCommands.js
+++
@@ -1,7 +0,0 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-import { Reminder } from './Commands/reminder.js';
-import { CommandHandler } from "./Handler/CommandHandler.js";
-(async () => {
-    await CommandHandler.install([Reminder.GetCommandData()]);
-})();
